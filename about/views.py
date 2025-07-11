5b9112981937d0e5c011358d6023bc80843bca5f
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-07-11 12:17:50 +0100

    complete collaboration form

--- about/views.py
+++ about/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import AboutSection
+from .forms import CollaborateForm
 
 # Create your views here.
 def get_about(request):
@@ -11,9 +12,11 @@ def get_about(request):
     :template:`about/about.html`
     """
     about = AboutSection.objects.first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about}
+        {"about": about,
+         "collaborate_form": collaborate_form},
     )
