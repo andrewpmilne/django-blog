0c6af28e39bfc083d6251ae3c172132d841ea622
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-07-11 13:24:56 +0100

    add colloboration form functionality

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import AboutSection
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 def get_about(request):
@@ -11,6 +12,12 @@ def get_about(request):
 
     :template:`about/about.html`
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)  
+        if collaborate_form.is_valid():                   
+            collaborate_form.save()
+            messages.success(request, "Your collaboration request has been submitted!")
+
     about = AboutSection.objects.first()
     collaborate_form = CollaborateForm()
 
