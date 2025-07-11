9caa47b8a49bcf7cae714fa60a4d33beb4791170
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-07-11 12:01:16 +0100

    begin to reate collaboration form on about

--- about/admin.py
+++ about/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import AboutSection
+from .models import AboutSection, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(AboutSection)
 class AboutSectionAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = ('title', 'updated_on')
     search_fields = ['title']
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+    list_display = ('message', 'read',)
